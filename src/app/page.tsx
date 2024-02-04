d05d8a3463084b6edc665809410c35b56823b706
khnbzkrt <khan.bozkurt@gmail.com>
2024-02-04 18:15:38 +0300

    branching ekleme anasayfa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,30 @@
 import BlogCard from "@/components/card/blog-card";
+import branchingImage from "/public/blog/branching.png";
 import sdlcImage from "/public/blog/sdlc.webp";
 
 const blogData = [
-  {
-    title: "SDLC (Yazılım Geliştirme Yaşam Döngüsü)",
-    content:
-      "Merhaba, bu yazıda SDLC (Software Development Life Cycle) kavramının sahip olduğu süreçleri ve süreçler içerisindeki detayları tecrübelerimle beraber aktarmaya çalışacağım.",
-    src: sdlcImage,
-    link: "/blog/yazilim-gelistirme-yasam-dongusu",
-  },
+    {
+        title: "SDLC (Yazılım Geliştirme Yaşam Döngüsü)",
+        content:
+            "Merhaba, bu yazıda SDLC (Software Development Life Cycle) kavramının sahip olduğu süreçleri ve süreçler içerisindeki detayları tecrübelerimle beraber aktarmaya çalışacağım.",
+        src: sdlcImage,
+        link: "/blog/yazilim-gelistirme-yasam-dongusu",
+    },
+    {
+        title: "Development Sürecinde Branching İşlemleri",
+        content:
+            "Merhaba, bu yazı içeriğinde size aktarmak istediğim konu, geliştirme sürecinde kullandığımız git repolarındaki branch sistemi. Pratikte branchler oluştururken hangi mantıkla nasıl bir yöntem izlediğimden bahsetmek istiyorum.",
+        src: branchingImage,
+        link: "/blog/development-surecinde-branching-islemleri",
+    },
 ];
 
 export default function Home() {
-  return (
-    <article className="grid grid-cols-1 gap-4">
-      {blogData.map((blog) => (
-        <BlogCard
-          key={blog.title}
-          link={blog.link}
-          src={blog.src}
-          title={blog.title}
-          content={blog.content}
-        />
-      ))}
-    </article>
-  );
+    return (
+        <article className="grid grid-cols-1 gap-4">
+            {blogData.map((blog) => (
+                <BlogCard key={blog.title} link={blog.link} src={blog.src} title={blog.title} content={blog.content} />
+            ))}
+        </article>
+    );
 }
