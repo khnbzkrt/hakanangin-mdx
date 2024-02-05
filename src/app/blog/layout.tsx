adc6685a97a5d9fee1000cc70bf5dcd0f4250b09
khnbzkrt <khan.bozkurt@gmail.com>
2024-02-05 19:22:48 +0300

    stil düzeltmeleri

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,10 +1,7 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div
-      className="grid grid-cols-[minmax(auto,900px)] place-content-center"
-      id="mdx-layout"
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div className="grid grid-cols-[minmax(auto,900px)] place-content-center" id="mdxLayout">
+            {children}
+        </div>
+    );
 }
--- src/components/card/blog-card.tsx
+++ src/components/card/blog-card.tsx
@@ -3,32 +3,26 @@ import Link from "next/link";
 import { FC } from "react";
 
 type BlogCardProps = {
-  title: string;
-  src: StaticImageData;
-  content: string;
-  link: string;
+    title: string;
+    src: StaticImageData;
+    content: string;
+    link: string;
 };
 
 const BlogCard: FC<BlogCardProps> = ({ src, title, content, link }) => {
-  return (
-    <Link href={link}>
-      <div className="grid grid-rows-[1fr] md:grid-cols-[200px,1fr] min-h-[200px] shadow-md rounded-2xl p-4 gap-4">
-        <div className="relative min-h-[300px] md:min-h-0">
-          <Image
-            src={src}
-            fill
-            loading="lazy"
-            alt={title}
-            className="object-center object-contain"
-          />
-        </div>
-        <div>
-          <h1>{title}</h1>
-          <p className="leading-8">{content}</p>
-        </div>
-      </div>
-    </Link>
-  );
+    return (
+        <Link href={link}>
+            <div className="grid grid-rows-[1fr] md:grid-cols-[200px,1fr] min-h-[200px] shadow-md rounded-2xl p-8 gap-8">
+                <div className="relative min-h-[300px] md:min-h-0">
+                    <Image src={src} fill loading="lazy" alt={title} className="object-center object-contain" />
+                </div>
+                <div>
+                    <h1>{title}</h1>
+                    <p className="leading-8">{content}</p>
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default BlogCard;
