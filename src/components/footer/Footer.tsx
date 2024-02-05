8b286ad362af6e3ce28a9deff0b59bcb83c85340
khnbzkrt <khan.bozkurt@gmail.com>
2024-02-06 00:10:34 +0300

    politikalar stil düzeltmesi

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <div className="flex items-center rounded-t-2xl  justify-evenly p-5 bg-gray-50 text-sm">
-            <Link href={"/contracts/privacy-policy"} className="text-neutral-600" target="_blank">
+        <div className="flex items-center rounded-t-2xl  justify-start p-3 text-sm gap-4 text-neutral-400">
+            <Link href={"/contracts/privacy-policy"} target="_blank">
                 Gizlilik Sözleşmesi
             </Link>
-            <Link href={"/contracts/terms-of-use"} className="text-neutral-600" target="_blank">
+            <Link href={"/contracts/terms-of-use"} target="_blank">
                 Kullanım Koşulları
             </Link>
-            <Link href={"/contracts/cookie-policy"} className="text-neutral-600" target="_blank">
+            <Link href={"/contracts/cookie-policy"} target="_blank">
                 Çerez Politikası
             </Link>
         </div>
