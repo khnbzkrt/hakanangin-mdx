c0a1b49703c2af7bea83a5e967adca811da36b78
khnbzkrt <khan.bozkurt@gmail.com>
2024-02-06 00:26:16 +0300

    refactor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,27 +2,31 @@ import AppLayout from "@/components/app-layout";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Hakan Angın Blog Sitesi",
-  description:
-    "Hakan Angın kişisel blog sitesi. Blog içeriğinde yazılım hayatımdaki tecrübelerimi paylaşıyorum.",
+    title: "Hakan Angın Blog Sitesi",
+    description: "Hakan Angın kişisel blog sitesi. Blog içeriğinde yazılım hayatımdaki tecrübelerimi paylaşıyorum.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AppLayout>{children}</AppLayout>
-      </body>
-      <GoogleAnalytics gaId="GTM-593634GH" />
-      <GoogleTagManager gtmId="G-QV271QJ8TW" />
-    </html>
-  );
+    return (
+        <html lang="en">
+            <Script
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7940059310086042"
+                crossOrigin="anonymous"
+            ></Script>
+            <body className={inter.className}>
+                <AppLayout>{children}</AppLayout>
+            </body>
+            <GoogleAnalytics gaId="GTM-593634GH" />
+            <GoogleTagManager gtmId="G-QV271QJ8TW" />
+        </html>
+    );
 }
